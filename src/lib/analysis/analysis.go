f255580acfddc42c259ca95feb97b4f456ea67a0
남궁현 <nkh@encoredtech.com>
2017-07-16 02:09:15 +0900

    added class, value, interface

--- src/lib/analysis/analysis.go
+++ src/lib/analysis/analysis.go
@@ -1,6 +1,7 @@
 package analysis
 
 import (
+  "fmt"
   "lib/model"
   "lib/languages/javascript"
 )
@@ -8,20 +9,32 @@ import (
 type KeywordMap map[string][]model.Keyword
 
 type Analyzer interface {
-  ReadValues(file model.File)
+  ReadValues(file model.File) []model.Keyword
   ReadFunctions(file model.File) []model.Keyword
-  ReadClasses(file model.File)
-  ReadInterfaces(file model.File)
+  ReadClasses(file model.File) []model.Keyword
+  ReadInterfaces(file model.File) []model.Keyword
 }
 
 func Analysis(p Analyzer, file model.File, keyword KeywordMap) {
-  /*
-  keywords := p.ReadFunctions(file)
-  for _, item := range keywords {
+  functionKeywords := p.ReadFunctions(file)
+  for _, item := range functionKeywords {
     keyword["function"] = append(keyword["function"], item)
   }
-  */
-  p.ReadValues(file)
+
+  valueKeywords := p.ReadValues(file)
+  for _, item := range valueKeywords {
+    keyword["value"] = append(keyword["value"], item)
+  }
+
+  classKeywords := p.ReadClasses(file)
+  for _, item := range classKeywords {
+    keyword["class"] = append(keyword["class"], item)
+  }
+
+  interfaceKeywords := p.ReadClasses(file)
+  for _, item := range interfaceKeywords {
+    keyword["interface"] = append(keyword["interface"], item)
+  }
 }
 
 func getLanguage(extension string) Analyzer {
@@ -50,4 +63,9 @@ func Start(scan model.Scan) {
     language := getLanguage(file.Ext)
     Analysis(language, file, keyword)
   }
+  fmt.Println()
+  fmt.Println("function", len(keyword["function"]))
+  fmt.Println("value", len(keyword["value"]))
+  fmt.Println("class", len(keyword["class"]))
+  fmt.Println("interface", len(keyword["interface"]))
 }
--- src/lib/languages/javascript/javascript.go
+++ src/lib/languages/javascript/javascript.go
@@ -1,11 +1,9 @@
 package javascript
 
 import (
-  "fmt"
   "lib/parser"
   "lib/model"
   "strings"
-  "regexp"
 )
 
 type Javascript struct {
@@ -37,33 +35,66 @@ func getFuncLine(line string, startIndex int, endIndex int) string {
 func isFuncLine(funcCode string) bool{
   return len(strings.Split(funcCode, " ")) == 2
 }
+
 func getFuncName(funcCode string) string {
   return string(strings.Split(funcCode, " ")[1])
 }
 
-func (p Javascript) ReadValues(file model.File) {
+func findValueStartIndex(line string, valueType string) int{
+  return strings.Index(line, valueType)
+}
+
+func findValueEndIndex(line string, startIndex int, valueEnds []string) int{
+  for k := 0; k < len(valueEnds); k++ {
+    valueEnd := valueEnds[k]
+    valueEndIndex := strings.Index(line, valueEnd)
+    if valueEndIndex > startIndex {
+      return valueEndIndex
+    }
+  }
+  return startIndex
+}
+
+func getValueLine(line string, startIndex int, endIndex int) []string{
+  typeCode := string(line[startIndex:endIndex])
+  tpyeCodes := strings.Split(typeCode, " ")
+  removedType := tpyeCodes[1: len(tpyeCodes)]
+  return removedType
+}
+
+func findTypeValue(line string, valueTypes []string, valueEnds []string) []string{
+  values := []string{}
+  for j := 0; j < len(valueTypes); j++ {
+    valueType := valueTypes[j]
+    valueStartIndex := findValueStartIndex(line, valueType)
+    valueEndIndex := findValueEndIndex(line, valueStartIndex, valueEnds)
+    if valueStartIndex > 0 && valueEndIndex > 0{
+      values = getValueLine(line, valueStartIndex, valueEndIndex)
+    }
+  }
+  return values
+}
+
+func (p Javascript) ReadValues(file model.File) []model.Keyword{
+  keywords := []model.Keyword{}
   code := parser.ReadSourceCode(file.Path)
   codes := strings.Split(code, "\n")
   valueTypes := []string{"var", "const", "let"}
   valueEnds := []string{"=", ","}
   for i := 0; i < len(codes); i++ {
     line := codes[i]
-    for j := 0; j < len(valueTypes); j++ {
-      valueType := valueTypes[j]
-      valueStartIndex := strings.Index(line, valueType)
-
-      for k := 0; k < len(valueEnds); k++ {
-        valueEnd := valueEnds[k]
-        valueEndIndex := strings.Index(line, valueEnd)
-
-        if valueStartIndex > 0 && valueEndIndex > 0{
-          fmt.Println("valueStartIndex", valueStartIndex, "line", line)
-          match, _ := regexp.MatchString(`([A-Za-z0-9\_]+)[ \t]{0,3}\=[^<>!]`, line)
-          fmt.Println(match)
+    values := findTypeValue(line, valueTypes, valueEnds)
+    if len(values) > 0 {
+      for j := 0; j < len(values); j++ {
+        keyword := model.Keyword {
+          Type: "value",
+          Name: values[j],
         }
+        keywords = append(keywords, keyword)
       }
     }
   }
+  return keywords
 }
 
 func (p Javascript) ReadFunctions(file model.File) []model.Keyword {
@@ -88,10 +119,60 @@ func (p Javascript) ReadFunctions(file model.File) []model.Keyword {
   return keywords
 }
 
-func (p Javascript) ReadClasses(file model.File) {
-  //fmt.Println("Javascript ReadClasses")
+
+func findClassStartIndex(line string) int{
+  return strings.Index(line, "class")
 }
 
-func (p Javascript) ReadInterfaces(file model.File) {
-  //fmt.Println("Javascript ReadInterfaces")
+func findClassEnIndex(line string, startIndex int) int{
+  for j := startIndex; j < len(line); j++ {
+    if string(line[j]) == "{" {
+      return j
+    }
+  }
+  return startIndex
+}
+
+func getClassLine(line string, startIndex int, endIndex int) string {
+  classCode := string(line[startIndex:endIndex])
+  classCodes := strings.Split(classCode, " ")
+  if len(classCode) > 1 && string(classCodes[1]) != "" {
+    return classCode
+  }
+  return ""
+}
+
+func isClassLine(classCode string) bool{
+  return len(strings.Split(classCode, " ")) == 2
+}
+
+func getClassName(classCode string) string {
+  return string(strings.Split(classCode, " ")[1])
+}
+
+func (p Javascript) ReadClasses(file model.File) []model.Keyword {
+  keywords := []model.Keyword{}
+  code := parser.ReadSourceCode(file.Path)
+  codes := strings.Split(code, "\n")
+  for i := 0; i < len(codes); i++ {
+    line := strings.Trim(codes[i], "")
+    classStartIndex := findClassStartIndex(line)
+    if classStartIndex == 0 {
+      classEndIndex := findClassEnIndex(line, classStartIndex)
+      classCode := getClassLine(line, classStartIndex, classEndIndex)
+      if isClassLine(classCode) {
+        keyword := model.Keyword {
+          Type: "class",
+          Name: getClassName(classCode),
+        }
+        keywords = append(keywords, keyword)
+      }
+    }
+  }
+  return keywords
+}
+
+func (p Javascript) ReadInterfaces(file model.File) []model.Keyword {
+  keywords := []model.Keyword{}
+  return keywords
 }
